2f6bb229c816b6733f2753fa28b6a3a755d4f356
ShubhaM180405 <sangram.23bce9368@vitapstudent.ac.in>
2025-12-14 20:31:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ================= DYNAMIC CITY GALLERY =================
 // MODIFICATION START
 
-function showGallery(cityId) {
+/*function showGallery(cityId) {
     document.querySelector('.city-selection').style.display = 'none';
 
     document.querySelectorAll('.gallery-section')
@@ -33,7 +33,50 @@ document.addEventListener("DOMContentLoaded", function () {
         popup.style.display = "none";
     });
 
-});
+});*/
+// ================= CAROUSEL GALLERY =================
+// MODIFICATION START
+
+const galleries = {
+    agra: ["taj1.jpg","taj2.jpg","taj3.jpg","taj4.jpg","taj5.jpg"],
+    delhi: ["delhi1.jpg","delhi2.jpg","delhi3.jpg","delhi4.jpg","delhi5.jpg"],
+    manali: ["manali1.jpg","manali2.jpg","manali3.jpg","manali4.jpg","manali5.jpg"],
+    jaipur: ["jaipur1.jpg","jaipur2.jpg","jaipur3.jpg","jaipur4.jpg","jaipur5.jpg"],
+    goa: ["goa1.jpg","goa2.jpg","goa3.jpg","goa4.jpg","goa5.jpg"],
+    kerala: ["kerala1.jpg","kerala2.jpg","kerala3.jpg","kerala4.jpg","kerala5.jpg"]
+};
+
+let currentCity = "";
+let currentIndex = 0;
+
+function openCarousel(city) {
+    currentCity = city;
+    currentIndex = 0;
+    document.getElementById("carouselPopup").style.display = "flex";
+    updateImage();
+}
+
+function closeCarousel() {
+    document.getElementById("carouselPopup").style.display = "none";
+}
+
+function updateImage() {
+    document.getElementById("carouselImage").src =
+        "images/" + galleries[currentCity][currentIndex];
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % galleries[currentCity].length;
+    updateImage();
+}
+
+function prevImage() {
+    currentIndex =
+        (currentIndex - 1 + galleries[currentCity].length) % galleries[currentCity].length;
+    updateImage();
+}
+
+// MODIFICATION END
 
 // MODIFICATION END
 // ================= ANIMATED COUNTERS =================
